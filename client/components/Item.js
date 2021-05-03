c9781dd827ebd2138b5aeb451128ad0dcc3937f1
Anthony Ingle <ingle.anthony@icloud.com>
2021-05-03 01:20:25 -0400

    Connected to backend. Able to manipulate items

--- client/components/Item.js
+++ client/components/Item.js
@@ -1,33 +1,96 @@
 import React from "react";
-import { View, StyleSheet, Text, TouchableHighlight } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  I18nManager,
+} from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { Swipeable } from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
 import { colors } from "../colors.js";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Item({ name, onPress, purchased }) {
+export default function Item({
+  id,
+  name,
+  purchased,
+  onPress,
+  onTriggerLeftSwipe,
+  onTriggerRightSwipe,
+  onRightOpen,
+}) {
   const checkboxRef = React.useRef();
 
-  const handlePress = async () => {
-    await onPress();
+  const onTriggerLeft = (progress) => {
+    let progressFloat = parseFloat(JSON.stringify(progress));
+    onTriggerLeftSwipe(id, { progress: progressFloat });
+  };
+
+  const onTriggerRight = () => {
+    onTriggerRightSwipe(id);
+  };
+
+  const renderLeftActions = (progress, dragX) => {
+    const trans = dragX.interpolate({
+      inputRange: [0, 50, 100, 101],
+      outputRange: [-20, 0, 0, 1],
+    });
+    onTriggerLeft(progress);
+    return (
+      <View style={styles.leftAction}>
+        <Animated.Text style={[styles.actionText]}>Claim</Animated.Text>
+      </View>
+    );
+  };
+
+  const renderRightActions = (dragX) => {
+    const trans = dragX.interpolate({
+      inputRange: [-101, -100, -50, 0],
+      outputRange: [1, 0, 0, -20],
+    });
+    return (
+      <View style={styles.rightAction}>
+        <Ionicons
+          name="trash"
+          size={24}
+          color="white"
+          style={styles.actionIcon}
+        />
+      </View>
+    );
   };
 
   return (
-    <TouchableHighlight
-      style={styles.touchable}
-      underlayColor={colors.green}
-      activeOpacity={0.6}
-      onPress={handlePress}
+    <Swipeable
+      renderLeftActions={renderLeftActions}
+      renderRightActions={renderRightActions}
+      containerStyle={styles.swipeable}
+      overshootLeft={false}
+      leftThreshold={680}
+      onSwipeableRightWillOpen={onTriggerRight}
+      onSwipeableRightOpen={() => onRightOpen(id)}
+      friction={1.7}
+      overshootFriction={4}
     >
-      <View style={styles.card}>
-        <BouncyCheckbox
-          text={name}
-          fillColor={colors.green}
-          iconStyle={{ borderColor: colors.green }}
-          isChecked={purchased}
-          disabled={true}
-          ref={checkboxRef}
-        />
-      </View>
-    </TouchableHighlight>
+      <TouchableHighlight
+        style={styles.touchable}
+        underlayColor={colors.green}
+        activeOpacity={0.6}
+        onPress={() => onPress(id, purchased)}
+      >
+        <View style={styles.card}>
+          <BouncyCheckbox
+            text={name}
+            fillColor={colors.green}
+            iconStyle={{ borderColor: colors.green }}
+            isChecked={purchased}
+            disabled={true}
+            ref={checkboxRef}
+          />
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 
@@ -43,11 +106,35 @@ const styles = StyleSheet.create({
   },
   touchable: {
     borderRadius: 24,
+  },
+  swipeable: {
+    borderRadius: 24,
     marginBottom: 8,
   },
   cardText: {
     fontSize: 20,
     fontWeight: "500",
     color: colors.dark,
+  },
+  leftAction: {
+    flex: 1,
+    backgroundColor: "#fcd703",
+    justifyContent: "center",
+  },
+  rightAction: {
+    alignItems: "center",
+    flexDirection: I18nManager.isRTL ? "row-reverse" : "row",
+    backgroundColor: "#dd2c00",
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  actionText: {
+    color: "white",
+    fontSize: 16,
+    backgroundColor: "transparent",
+    paddingHorizontal: 20,
+  },
+  actionIcon: {
+    marginHorizontal: 20,
   },
 });
--- client/screens/GroceryListScreen.js
+++ client/screens/GroceryListScreen.js
@@ -1,57 +1,108 @@
 import React from "react";
-import { SafeAreaView, SectionList, StyleSheet, Text } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  SectionList,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+} from "react-native";
 import Item from "../components/Item";
+import NewItem from "../components/NewItem";
 import { colors } from "../colors.js";
-import { useQuery } from "@apollo/client";
-import { get_list } from "../graphql";
+import { useLazyQuery, useMutation } from "@apollo/client";
+import { get_list, add_item, purchase_item, remove_item } from "../graphql.js";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { AntDesign } from "@expo/vector-icons";
+import * as Haptics from "expo-haptics";
 
 const DATA = [
   {
-    data: [{ id: "1235124123442", name: "carrots", purchased: false }],
+    data: [],
   },
   {
     title: "Purchased",
-    data: [{ id: "3242141234", name: "oats", purchased: true }],
+    data: [],
   },
 ];
-
-function sortData(receievedData) {
-  for (let i = 0; i < receievedData.get_list.items.length; i++) {
-    let item = receievedData.get_list.items[i];
-    console.log(item);
-    let exists = false;
-
-    //TEMP ERROR CHECK
-    for (let j = 0; j < DATA[1].data.length; j++) {
-      if (DATA[1].data[j].id == item.id) exists = true;
-    }
-
-    for (let j = 0; j < DATA[0].data.length; j++) {
-      if (DATA[0].data[j].id == item.id) exists = true;
-    }
-    //TEMP ERROR CHECK
-
-    if (!exists) {
-      if (item.purchased) DATA[1].data.push(item);
-      else DATA[0].data.push(item);
-    }
-  }
-}
-
-function purchaseItem() {}
 
 export default function ({ route, navigation }) {
   // refreshes SectionList state (could be temporary fix)
   const [refresh, setRefresh] = React.useState(false);
+  const [listName, setListName] = React.useState("");
+  const [adding, setAdding] = React.useState("");
+  const [items, setItems] = React.useState([
+    { data: [{ id: 1234, name: "beef", purchased: false }] },
+    { title: "Purchased", data: [] },
+  ]);
+
+  const listRef = React.useRef();
 
   // gets id and name from the join or create screen
   // const { listID, listName } = route.params;
-  const tempName = "Test List";
-  const tempID = "6085efe2fd203d0c80907c18";
+  const tempID = "608c3d535cbba308d0fecbcf";
+  const userID = "608b5ddddb8f322bbc238bca";
 
-  const { loading, error, data } = useQuery(get_list, {
+  const sortItems = (data, queryName) => {
+    const itemsArray = data[queryName].items;
+    const unPurchasedItems = itemsArray.filter((item) => !item.purchased);
+    const PurchasedItems = itemsArray.filter((item) => item.purchased);
+    setItems([
+      { data: unPurchasedItems },
+      { title: items[1].title, data: PurchasedItems },
+    ]);
+  };
+
+  const createNewItem = () => {
+    const newItem = {
+      id: Math.random(),
+      new: true,
+    };
+    setItems([
+      { data: [...items[0].data, newItem] },
+      { title: items[1].title, data: items[1].data },
+    ]);
+    listRef.current.scrollToLocation({
+      itemIndex: items[0].data.length,
+      sectionIndex: 0,
+      viewPosition: 0.5,
+    });
+  };
+
+  // graphql
+  const [getList, { loading, error, data }] = useLazyQuery(get_list, {
     variables: { listID: tempID },
+    onCompleted: () => {
+      setListName(data.get_list.list_name);
+      sortItems(data, "get_list");
+    },
   });
+
+  const [addItem] = useMutation(add_item, {
+    update(proxy, result) {
+      const latestItem = result.data.add_item.items.pop();
+      setItems([
+        { data: [...items[0].data.filter((i) => !i.new), latestItem] },
+        { title: items[1].title, data: items[1].data },
+      ]);
+    },
+  });
+
+  const [purchaseItem] = useMutation(purchase_item, {
+    update(proxy, result) {
+      sortItems(result.data, "purchase_item");
+    },
+  });
+
+  const [removeItem] = useMutation(remove_item, {
+    update(proxy, result) {
+      sortItems(result.data, "remove_item");
+    },
+  });
+
+  React.useEffect(() => {
+    getList();
+  }, []);
 
   if (loading)
     return (
@@ -61,46 +112,101 @@ export default function ({ route, navigation }) {
     );
 
   if (error) console.log(error);
-  if (!loading) sortData(data);
 
-  // move item to purchase section
-  const setPurchased = (item) => {
-    let arr = DATA[0].data;
-    if (arr.indexOf(item) == -1) return;
-    let spliced = arr.splice(arr.indexOf(item), 1);
-    DATA[1].data.push(spliced[0]);
-    setRefresh(!refresh);
+  // const updateCache = (cache, { data }) => {
+  //   // Fetch the todos from the cache
+  //   const existingTodos = cache.readQuery({
+  //     query: get_list,
+  //   });
+  //   console.log(cache);
+  //   console.log(data);
+  // };
 
-    return spliced[0];
+  function onAdd() {
+    if (adding === "") {
+      setItems([
+        { data: [...items[0].data.filter((i) => !i.new)] },
+        { title: items[1].title, data: items[1].data },
+      ]);
+    } else {
+      addItem({ variables: { name: adding, listID: tempID, userID } });
+      setAdding("");
+    }
+  }
+
+  function onPurchase(id, purchased) {
+    let method = purchased ? "unpurchase" : "purchase";
+    purchaseItem({ variables: { listID: tempID, itemID: id, userID, method } });
+  }
+
+  // pass state in when changing text when adding new item
+  function onChangeAdd(text) {
+    setAdding(text);
+  }
+
+  function onTriggerRightSwipe(id) {
+    Haptics.impactAsync("medium");
+  }
+
+  function onRightOpen(id) {
+    removeItem({ variables: { listID: tempID, itemID: id, userID } });
+  }
+
+  function onTriggerLeftSwipe(id, { progress }) {
+    if (progress >= 0.2) {
+      console.log(`claim ${id}`);
+      Haptics.impactAsync("medium");
+    }
+  }
+
+  // renders SectionList (newitem is rendered when adding a new item)
+  const renderItem = ({ item }) => {
+    return item.new ? (
+      <NewItem
+        id={items[0].data.length}
+        onChangeAdd={onChangeAdd}
+        onAdd={onAdd}
+      />
+    ) : (
+      <Item
+        id={item.id}
+        name={item.name}
+        onPress={(id, purchased) => onPurchase(id, purchased)}
+        purchased={item.purchased}
+        onTriggerLeftSwipe={(id, progress) => onTriggerLeftSwipe(id, progress)}
+        onTriggerRightSwipe={(id) => onTriggerRightSwipe(id)}
+        onRightOpen={onRightOpen}
+      />
+    );
   };
-
-  // renders SectionList
-  const renderItem = ({ item }) => (
-    <Item
-      id={item.id}
-      name={item.name}
-      onPress={() => {
-        setRefresh(!refresh);
-        console.log(DATA);
-      }}
-      purchased={item.purchased}
-    />
-  );
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>{tempName}</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <Text style={styles.title}>{listName}</Text>
       <SectionList
-        sections={DATA}
+        sections={items}
         renderItem={renderItem}
         renderSectionHeader={({ section: { title } }) =>
           title && <Text style={styles.heading}>{title}</Text>
         }
         keyExtractor={(item) => item.id}
-        extraData={refresh}
         stickySectionHeadersEnabled={false}
+        ref={listRef}
       />
-    </SafeAreaView>
+      <View style={styles.absolute}>
+        <TouchableHighlight
+          style={styles.addButton}
+          onPress={createNewItem}
+          underlayColor={colors.light}
+        >
+          <View style={styles.addButton}>
+            <AntDesign name="plus" style={styles.plus} />
+          </View>
+        </TouchableHighlight>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -117,7 +223,7 @@ const styles = StyleSheet.create({
     fontWeight: "800",
     color: colors.green,
     marginBottom: 12,
-    marginTop: 24,
+    marginTop: 60,
   },
   heading: {
     fontSize: 28,
@@ -132,5 +238,22 @@ const styles = StyleSheet.create({
     paddingTop: 80,
     paddingBottom: 300,
     color: colors.dark,
+  },
+  addButton: {
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    backgroundColor: colors.green,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  plus: {
+    fontSize: 45,
+    color: colors.light,
+  },
+  absolute: {
+    position: "absolute",
+    bottom: 40,
+    right: 40,
   },
 });
